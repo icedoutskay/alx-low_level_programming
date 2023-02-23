dcb0b4dac0c6d2f2d227286de49efb5a1fabf9e7
sommie <somkeneodikpo@gmail.com>
2023-02-23 08:42:53 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,17 +7,17 @@
 
 void print_most_numbers(void)
 {
-	char n = 0;
-        void _putchar ();
-	while (n <= 9)
-	{
-		if (n != 2 && n != 4)
-		{
-			_putchar('0' + n);
-		}
+char n = 0;
+void _putchar (void);
+while (n <= 9)
+{
+if (n != 2 && n != 4)
+{
+_putchar('0' + n);
+}
 
-		n++;
-	}
-	_putchar('\n');
+n++;
+}
+_putchar('\n');
 
 }
