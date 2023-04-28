adc1798deb1edd66311cbdd1f3af79afa26fadeb
sommie <somkeneodikpo@gmail.com>
2023-04-27 20:40:42 -0700

    ghbnksdnh

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,16 +2,15 @@
 
 /**
  * listint_len - Returns the number of elements in a linked listint_t list.
+ * @h: Pointer to the head of the list.
  *
- * @h: Pointer to the head node of list.
- *
- * Return: Number of elements in list.
+ * Return: The number of nodes in the list.
  */
 size_t listint_len(const listint_t *h)
 {
 	size_t count = 0;
 
-	while (h)
+	while (h != NULL)
 	{
 		h = h->next;
 		count++;
